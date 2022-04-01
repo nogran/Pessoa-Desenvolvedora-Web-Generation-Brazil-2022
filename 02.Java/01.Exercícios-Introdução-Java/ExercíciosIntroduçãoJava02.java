ee4f98b4888f78f2e9caca9a6d590714ce7b0f99
Mateus Nogueira <50894389+nogran@users.noreply.github.com>
2022-04-01 20:19:35 -0300

    ex02 done

---
+++ 02.Java/01.Exercícios-Introdução-Java/ExercíciosIntroduçãoJava02.java
@@ -0,0 +1,24 @@
+/* 2. Faça um sistema que leia a idade de uma pessoa expressa em dias e
+ * mostre-a expressa em anos, meses e dias.
+ */
+
+import java.util.Scanner;
+
+public class ExercíciosIntroduçãoJava02 {
+
+	public static void main(String[] args) {
+
+		int idadeDias;
+		int totalAnos;
+		int totalMeses;
+
+		Scanner leia = new Scanner(System.in);
+		System.out.print("Digite a quantidade de dias: ");
+		idadeDias = leia.nextInt();
+		totalAnos = idadeDias / 365;
+		totalMeses = idadeDias / 30;
+		System.out.println("A idadde em anos é: " + totalAnos);
+		System.out.println("A idadde em meses é: " + totalMeses);
+		System.out.println("A idadde em dias é: " + idadeDias);
+	}
+}
