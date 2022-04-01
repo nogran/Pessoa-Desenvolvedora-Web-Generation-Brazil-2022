61199d1ef5cb6ef04c5ee13bcb422e85135079a1
Mateus Nogueira <50894389+nogran@users.noreply.github.com>
2022-04-01 20:03:07 -0300

    ex01 done

--- 02.Java/01.Exercícios-Introdução-Java/ExercíciosIntroduçãoJava01.java
+++ 02.Java/01.Exercícios-Introdução-Java/ExercíciosIntroduçãoJava01.java
@@ -1,3 +1,8 @@
+/*
+ * 1. Faça um sistema que leia a idade de uma pessoa expressa em anos,
+ * meses e dias e mostre-a expressa apenas em dias.
+ */
+
 import java.util.Scanner;
 
 public class ExercíciosIntroduçãoJava01 {
