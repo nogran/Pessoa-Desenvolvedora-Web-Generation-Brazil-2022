b560b28aedc67d5433b469ffcdb0061bb1d16a24
Mateus Nogueira <50894389+nogran@users.noreply.github.com>
2022-04-01 20:55:21 -0300

    ex03 done

---
+++ 02.Java/01.Exercícios-Introdução-Java/ExercíciosIntroduçãoJava03.java
@@ -0,0 +1,32 @@
+/*
+ * 3. Faça um sistema que leia o tempo de duração de um evento em uma fábrica
+ * expressa em segundos e mostre-o expresso em horas, minutos e segundos.
+ */
+
+import java.util.Scanner;
+
+public class ExercíciosIntroduçãoJava03 {
+	static public void main(String[] args) {
+
+		int segundos;
+		int minutos;
+		int horas;
+
+		horas = 0;
+		minutos = 0;
+		Scanner leia = new Scanner(System.in);
+		System.out.print("Digite a duração em segundos: ");
+		segundos = leia.nextInt();
+		if (segundos >= 60) {
+			minutos = segundos / 60;
+			segundos = segundos % 60;
+			if (minutos >= 60) {
+				horas = minutos / 60;
+				minutos = minutos % 60;
+			}
+		}
+		System.out.println("A duração do evento foi de: " + horas +
+				" horas, " +  minutos + " minutos e de " + segundos +
+				" segundos");
+	}
+}
