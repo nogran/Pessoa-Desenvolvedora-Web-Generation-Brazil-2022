f66b098edc5a3ee341221f4bd28d3d631ef65337
Mateus Nogueira <50894389+nogran@users.noreply.github.com>
2022-04-02 22:03:46 -0300

    ex01 done

---
+++ 02.Java/02.Laços-Condicionais/ExercíciosJavaLaçosCondicionais01.java
@@ -0,0 +1,30 @@
+/*
+ * 1. Faça um programa que receba três inteiros e diga qual deles é o maior.
+ */
+
+import java.util.Scanner;
+
+public class ExercíciosJavaLaçosCondicionais01 {
+	static public void main(String[] args) {
+		int a;
+		int b;
+		int c;
+		int max;
+
+		Scanner leia = new Scanner(System.in);
+		System.out.print("Digite o primeiro número: ");
+		a = leia.nextInt();
+		max = a;
+		System.out.print("Digite o segundo número: ");
+		b = leia.nextInt();
+		if(b > a) {
+			max = b;
+		}	
+		System.out.print("Digite o terceiro número: ");
+		c = leia.nextInt();
+		if(c > b && c > a) {
+			max = c;
+		}	
+		System.out.println("O maior número é: " + max);
+	}
+}
