7ffdc3636a2c0b47826669c8f47cc7df00b5a32f
Mateus Nogueira <50894389+nogran@users.noreply.github.com>
2022-04-06 22:19:10 -0300

    text add

--- 02.Java/05.Orientação-a-Objetos/AtividadeClasseObjetoJava01/Cliente.java
+++ 02.Java/05.Orientação-a-Objetos/AtividadeClasseObjetoJava01/Cliente.java
@@ -1,3 +1,8 @@
+/*
+ * 1. Crie uma classe cliente e apresente os atributos e métodos referentes esta classe,
+ * em seguida crie um objeto cliente, defina as instancias deste objeto e apresente as
+ * informações deste objeto no console. 
+ */
 
 import java.util.Scanner;
 
