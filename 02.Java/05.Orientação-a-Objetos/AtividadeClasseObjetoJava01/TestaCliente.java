31148c6e6cf3d18003e58d83150ada706a901e19
Mateus Nogueira <50894389+nogran@users.noreply.github.com>
2022-04-06 21:09:48 -0300

    ex01 done

--- 02.Java/05.Orientação-a-Objetos/AtividadeClasseObjetoJava01/TestaCliente.java
+++ 02.Java/05.Orientação-a-Objetos/AtividadeClasseObjetoJava01/TestaCliente.java
@@ -1,24 +1,21 @@
 
 public class TestaCliente {
-
+	
 	public static void main(String[] args) {
+		Cliente cliente1 = new Cliente();
+		cliente1.nomeCliente = "Mateus";
+		cliente1.enderecoCliente = "Rua das Rosas, 121";
+		cliente1.cidadeCliente = "Sao Paulo";
+		cliente1.estadoCliente = "SP";
+		cliente1.cpfCliente = "000.000.000-00";
 		
-	    Cliente cliente1 = new Cliente();
-	    
-	    cliente1.nomeCliente = "Mateus";
-	    cliente1.enderecoCliente = "Rua das Rosas, 121";
-	    cliente1.cidadeCliente = "Sao Paulo";
-	    cliente1.estadoCliente = "SP";
-	    cliente1.cpfCliente = "000.000.000-00";
-	    
-	    cliente1.imprimirInfo();
-	    System.out.println("\n-----------Alterando-----------\n");
-	    cliente1.alterarNome();
-	    cliente1.imprimirInfo();
-	    System.out.println("\n-----------Digitando-----------\n");
-	    cliente1.digitar();
-	    System.out.println("\n-----------Novo Nome-----------\n");
-	    cliente1.imprimirInfo();
-	    
+		cliente1.imprimirInfo();
+		System.out.println("\n-----------Alterando-----------\n");
+		cliente1.alterarNome();
+		cliente1.imprimirInfo();
+		System.out.println("\n-----------Digitando-----------\n");
+		cliente1.digitar();
+		System.out.println("\n-----------Novo Nome-----------\n");
+		cliente1.imprimirInfo();
 	}
 }
