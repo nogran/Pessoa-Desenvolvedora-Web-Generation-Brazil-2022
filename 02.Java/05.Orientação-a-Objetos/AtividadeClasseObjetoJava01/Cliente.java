7271b2d8c684eb5d7a5fe98802afcf0edf374fdc
Mateus Nogueira <50894389+nogran@users.noreply.github.com>
2022-04-06 21:04:01 -0300

    ex01 done

---
+++ 02.Java/05.Orientação-a-Objetos/AtividadeClasseObjetoJava01/Cliente.java
@@ -0,0 +1,30 @@
+
+import java.util.Scanner;
+
+public class Cliente {
+	
+	String nomeCliente;
+	String enderecoCliente;
+	String cidadeCliente;
+	String estadoCliente;
+    String cpfCliente;
+    
+    void imprimirInfo() {
+    	System.out.println("Cliente: " + nomeCliente);
+	    System.out.println("Endereco: " + enderecoCliente);
+	    System.out.println("Cidade: " + cidadeCliente);
+	    System.out.println("Estado: " + estadoCliente);
+	    System.out.println("CPF: " + cpfCliente);
+    }
+    
+    void alterarNome() {
+    	nomeCliente = "Mateus Nogueira";
+    }
+    
+    void digitar() {
+    	Scanner leia = new Scanner(System.in);
+    	System.out.print("Digite o novo nome: ");
+    	nomeCliente = leia.next();		
+    }
+
+}
