cb0000cde203844977786e9027e874661e54501c
Mateus Nogueira <50894389+nogran@users.noreply.github.com>
2022-04-20 13:34:00 -0300

    renamed folder and ex01 done

---
+++ 03.Banco-de-Dados/02.Atividade-de-banco-de-dados-relacional-Mysql/exe01.sql
@@ -0,0 +1,24 @@
+-- 1. Crie um banco de dados para um serviço de RH de uma empresa,
+-- onde o sistema trabalhará com as informações dos colaboradores
+-- desta empresa. 
+
+CREATE DATABASE db_rh;
+
+USE db_rh;
+
+CREATE TABLE tb_user(
+id BIGINT AUTO_INCREMENT,
+nome VARCHAR(20) NOT NULL,
+sobrenome VARCHAR(30) NOT NULL,
+salario FLOAT NOT NULL,
+ddd INT(2) NOT NULL,
+telefone VARCHAR(25) NOT NULL,
+data_nasc DATE NULL,
+admissao DATE NOT NULL,
+email VARCHAR(30) NULL,
+primary key(id)
+);
+
+SELECT * FROM tb_user;
+
+
