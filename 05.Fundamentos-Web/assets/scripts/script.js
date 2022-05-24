ac6c72db3383a076e0df338bac5db82938c87ba3
Mateus Nogueira <50894389+nogran@users.noreply.github.com>
2022-05-23 23:42:28 -0300

    email validacao indexOf

--- 05.Fundamentos-Web/assets/scripts/script.js
+++ 05.Fundamentos-Web/assets/scripts/script.js
@@ -2,6 +2,7 @@ let nome = window.document.getElementById('nome')
 let email = document.getElementById('email')
 let assunto = document.getElementsByName('assunto')
 
+
 function verifica(){
     if(nome.value == "" || email.value == "" || assunto.value == ""){
         alert("Todos os campos são obrigatórios")
@@ -9,6 +10,15 @@ function verifica(){
     }
     return true
 }
+
+function verifica_email(){
+    if (email.value.indexOf('@') == -1 || email.value.indexOf('.') == -1){
+        alert("Digite email valido")
+        return false
+    }
+    return true
+}
+//-1 se não existir na array ou string o valor procurado
 
 function enviar(){
     if(verifica()){
